bd063c39e0ae883c037274b4edba21fff29261a4
kotatsumuri <yyqpp621@gmail.com>
2019-05-15 20:44:10 +0900

    second commit !!!

--- server.py
+++ server.py
@@ -39,11 +39,13 @@ def callback():
     return 'OK'
 
 
+'''
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+'''
 
 @handler.add(FollowEvent)
 def handle_follow(event):
